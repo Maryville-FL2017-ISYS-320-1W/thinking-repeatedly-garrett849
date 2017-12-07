0c59cfa7b57546d2651e8fe409872caddfdfff82
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-12-07 05:18:54 -0600

    numbersright

--- src/P13_NumbersRight.java
+++ src/P13_NumbersRight.java
@@ -8,6 +8,16 @@ public class P13_NumbersRight {
 
 	public static void main(String[] args) {
 
+		int rows = 5;
+		for (int line = 1; line <= rows; line++) {
+			for (int spacesIndex = 1; spacesIndex <= rows - line; spacesIndex++) {
+				System.out.print(" ");
+			}
+			for (int numbersIndex = 1; numbersIndex <= line; numbersIndex++) {
+				System.out.print(line);
+			}
+			System.out.println();
+		}
 	}
 
 }
