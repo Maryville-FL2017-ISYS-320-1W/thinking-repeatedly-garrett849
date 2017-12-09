2e6f56e20dd56db96421cbb1cb93b33f6df64a46
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-12-09 03:44:36 -0600

    count

--- src/P1_Count5.java
+++ src/P1_Count5.java
@@ -5,9 +5,9 @@
 */
 
 public class P1_Count5 {
-    public static void main(String[] args) {
-        for( int i = /* FINISH ME */ ) {
-            System.out.println( /* FINISH ME */);
-        }
-    }
+	public static void main(String[] args) {
+		for (int i = 1; i < 5; i++) {
+			System.out.println("5 times 1" + i);
+		}
+	}
 }
