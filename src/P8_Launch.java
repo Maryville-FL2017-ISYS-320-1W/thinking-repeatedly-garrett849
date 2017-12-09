cdeac428245671f691ca52f56a27c86be2bfff6f
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-12-09 03:44:13 -0600

    launch

--- src/P8_Launch.java
+++ src/P8_Launch.java
@@ -5,17 +5,31 @@
 */
 
 // 1. Your predicted output 
-
+/*
+ T-minus
+ 5,4,3,2,1
+ Blastoff
+ 
+*/
 
 
 public class P8_Launch {
-
-	public static void main(String[] args) {
-
-	}
-
+    public static void main( String[] args ) {
+        System.out.println("T-minus ");
+        for( int i = 5; i >= 1; i-- ) {
+            System.out.print( i + ", " );
+        }
+        System.out.println("Blastoff!");
+    }
 }
 
 
 
 // 3. Were you correct? Explain any differences and what you learned.
+/*
+ I was half right blastoff went on the same line as the count down because it was a print not println.
+ */
+
+
+
+
