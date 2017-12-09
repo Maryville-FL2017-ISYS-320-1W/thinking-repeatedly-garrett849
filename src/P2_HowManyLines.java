c4236bc4b9ac38a47bdff813e8b3d9371cb7df38
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-12-09 03:31:05 -0600

    Howmanylines

--- src/P2_HowManyLines.java
+++ src/P2_HowManyLines.java
@@ -4,18 +4,22 @@
   	Date: 
 */
 
-// 1. Your predicted output 
-
-
-
+/* 1. Your predicted output 
+	How many lines
+	are printed ?
+	How many lines
+	are printed ?
+*/
 public class P2_HowManyLines {
 
 	public static void main(String[] args) {
-
+		for( int i = 1; i <= 3; i++ ) 
+		    System.out.println( "How many lines" );
+		    System.out.println( "are printed?" );
 	}
 
 }
+/*	I was wrong the loop was only for the first line not the second line. 
 
-
-
+*/
 // 3. Were you correct? Explain any differences and what you learned.
